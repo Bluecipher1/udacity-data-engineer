6625568161f63a04992deab3fe8aee2295e6e33e
Hauke Hildebrandt <hauke.hildebrandt@bluecipher.de>
2020-12-07 21:09:47 +0100

    Added docstrings

--- data-warehouse/create_tables.py
+++ data-warehouse/create_tables.py
@@ -4,12 +4,26 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """
+    Description: Drops all tables from the schema
+    
+    Arguments:
+        cur: the database connection cursor
+        conn: the database connection
+    """    
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """
+    Description: Creates database schema
+    
+    Arguments:
+        cur: the database connection cursor
+        conn: the database connection
+    """  
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
