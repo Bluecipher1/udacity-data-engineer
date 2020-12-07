5dac2fe3b84167a18a8bf9a38c0a1f0a6df8983f
Hauke Hildebrandt <hauke.hildebrandt@bluecipher.de>
2020-12-07 21:07:31 +0100

    Added docstrings

--- data-warehouse/etl.py
+++ data-warehouse/etl.py
@@ -4,12 +4,26 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    Description: This function manages the loading of data files from S3 to staging tables.
+    
+    Arguments:
+        cur: the database connection cursor
+        filepath: the path to the song file
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+        """
+    Description: This function loads the data from the staging area into the star schema.
+    
+    Arguments:
+        cur: the database connection cursor
+        filepath: the path to the song file
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
